79637f733a88a8bbf9dcb21dcba2d971b4397578
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-01-27 04:16:11 -0800

    add scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
