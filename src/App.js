cdd1cb8f313a8125939f5771c21053de49d3dfa8
Matthew Stewart <matthew.stewart.mi@gmail.com>
2019-01-28 12:08:05 -0800

    add viewport container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path="/about" render={(props) => (<div>About Page</div>)} />
-          <Route exact path="/login" render={(props) => (<div>Login Page</div>)} />
-          <Route exact path="/signup" render={(props) => (<div>Signup Page</div>)} />
-          <Route exact path="/profile" render={(props) => (<div>User Profile Page</div>)} />
-          <Route exact path="/" render={(props) => (<div>Landing Page</div>)} />
-        </Switch>
+        <div className="viewport-container">
+          <Switch>
+            <Route exact path="/about" render={(props) => (<div>About Page</div>)} />
+            <Route exact path="/login" render={(props) => (<div>Login Page</div>)} />
+            <Route exact path="/signup" render={(props) => (<div>Signup Page</div>)} />
+            <Route exact path="/profile" render={(props) => (<div>User Profile Page</div>)} />
+            <Route exact path="/" render={(props) => (<div>Landing Page</div>)} />
+          </Switch>
+        </div>
       </div>
     );
   }
